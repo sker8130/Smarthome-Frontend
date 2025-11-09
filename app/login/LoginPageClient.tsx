0d67d7aa3f1266f2da0f6f38352ca60c0f97bf72
TinBKU <tin.tontrong@hcmut.edu.vn>
2025-11-09 23:38:08 +0700

    fix-dashboard-va-api
    - Thêm 2 biểu đồ lịch sử ánh sáng, nhiệt độ
    - Kết nối api login, register

--- app/login/LoginPageClient.tsx
+++ app/login/LoginPageClient.tsx
@@ -44,7 +44,7 @@ export default function LoginPage() {
     e.preventDefault();
     setLoading(true);
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+      const res = await fetch(`http://localhost:3000/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
